d1965ac8c69347f64a13be9032eb58138f04875d
himal999 <himal.official99@gmail.com>
2022-08-28 11:08:08 +0530

    document update

--- controller/user.js
+++ controller/user.js
@@ -93,4 +93,66 @@ const createCustomer = (req, res) => {
     });
 };
 
-export { getAllCustomer, createCustomer, findCustomer };
+const updateCustomer = (req, res) => {
+  const id = req.params.id;
+  const user = req.body;
+  if (!user.firstName) {
+    return res.status(400).send({
+      message: 'Not Null First Name',
+    });
+  }
+  if (!user.sureName) {
+    return res.status(400).send({
+      message: 'Not Null Sure Names',
+    });
+  }
+  if (!user.gender) {
+    return res.status(400).send({
+      message: 'Not Null gender',
+    });
+  }
+  if (!user.dob) {
+    return res.status(400).send({
+      message: 'Not Null dob',
+    });
+  }
+  if (!user.password) {
+    return res.status(400).send({
+      message: 'Not Null password',
+    });
+  }
+  if (!user.telNo) {
+    return res.status(400).send({
+      message: 'Not Null telNo',
+    });
+  }
+  if (!user.email) {
+    return res.status(400).send({
+      message: 'Not Null email',
+    });
+  }
+  Scheme.findByIdAndUpdate(id, {
+    firstName: user.firstName,
+    sureName: user.sureName,
+    gender: user.gender,
+    dob: user.dob,
+    password: user.password,
+    telNo: user.telNo,
+    email: user.email,
+  })
+    .then((data) => {
+      if (!data) {
+        return res.status(404).send({
+          message: "Can't Find or update customer",
+        });
+      }
+      res.status(200).send(data);
+    })
+    .catch((error) => {
+      res.status(500).send({
+        message: 'Not Found',
+      });
+    });
+};
+
+export { getAllCustomer, createCustomer, findCustomer, updateCustomer };
--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,13 @@ import {
   getAllCustomer,
   createCustomer,
   findCustomer,
+  updateCustomer,
 } from '../controller/user.js';
 const route = express.Router();
 
 route.get('/', getAllCustomer);
 route.get('/:id', findCustomer);
 route.post('/createUser', createCustomer);
+route.put('/updateCustomer/:id', updateCustomer);
 
 export default route;
